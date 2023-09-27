8bff408b287945544152e7981fa0cdffd42acb5c
DayanandBotla-PS <dayanand@pragmatic.solutions>
2023-09-27 07:45:47 +0530

    getin page

---
+++ src/app/get-in/get-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-in',
+  templateUrl: './get-in.component.html',
+  styleUrls: ['./get-in.component.scss']
+})
+export class GetInComponent {
+
+}
